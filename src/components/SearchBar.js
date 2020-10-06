61ce0aada31db969da5f5cb2f91fe718f90787b5
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-10-06 15:20:03 +0530

    component created with material ui and handled state as well

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import { Paper, TextField } from '@material-ui/core';
+
 class SearchBar extends React.Component {
+
+    state = {
+        searchTerm: '',
+    }
+
+    handleChange = (event) => {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+    }
+
     render() {
         return (
-            <h1> This is search bar </h1>
+            <Paper elevation={6} style={{ padding: '25px' }}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField fullWidth label="Search..." onChange={this.handleChange} />
+                </form>
+            </Paper>
         )
     }
 }

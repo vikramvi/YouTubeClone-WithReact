8e810f918397090af418fdb637ea3aaf45eda457
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-10-07 11:42:09 +0530

    added lifecycle method to load default data

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ class App extends React.Component {
         selectedVideo: null,
     }
 
+    componentDidMount() {
+        this.handleSubmit('meditation');
+    }
+
     onVideoSelect = (video) => {
         this.setState({ selectedVideo: video });
     }
